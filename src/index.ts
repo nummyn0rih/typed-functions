787047310373a34720ccc9ec79b1fe6292836526
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-07-09 23:32:41 +0300

    feat: setup eslint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import totalPrice from './totalPrice.js'
 
-const test = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 })
-
-console.log(test)   // 6250
+console.log('base', totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 })) // 6250
+console.log('discount 0, months 10', totalPrice({ price: 100000, discount: 0, isInstallment: true, months: 10 })) // 10000
+console.log('discount 10', totalPrice({ price: 100000, discount: 10, isInstallment: true, months: 12 })) // 7500
+console.log('months 6', totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 6 })) // 12500
+console.log('isInstallment false', totalPrice({ price: 100000, discount: 25, isInstallment: false, months: 12 })) // 75000
+console.log('discount 0, isInstallment false', totalPrice({ price: 100000, discount: 0, isInstallment: false, months: 12 })) // 100000
--- src/totalPrice.ts
+++ src/totalPrice.ts
@@ -1,4 +1,4 @@
-import { type Params } from "./interfaces.js";
+import { type Params } from './interfaces.js'
 
 export default ({ price, discount, isInstallment, months }: Params): number => {
   const discountPrice: number = price - price * discount / 100
